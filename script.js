452df83adb534a966326278d8036a5278a0177b7
Nathan Montelli <nathanmontelli@gmail.com>
2021-12-31 13:53:19 -0800

    added for loop for past, present, and future

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const currentDate = moment().format('MMMM Do YYYY')
 $('#currentDay').text(currentDate)
+const currentHour = moment().hour()
+console.log(currentHour)
+
+for(let i = 9; i < 18; i++) {
+  if (currentHour < i) {
+    document.getElementById(i).classList.add('past')
+  } else if (currentHour === i) {
+    document.getElementById(i).classList.add('present')
+  } else {
+    document.getElementById(i).classList.add('future')
+  }
+} 
