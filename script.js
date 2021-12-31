2c00fe4608b7a53f82ee8b51439d055d7eec76d7
Nathan Montelli <nathanmontelli@gmail.com>
2021-12-31 15:57:19 -0800

    formatted and changed text color

--- script.js
+++ script.js
@@ -2,17 +2,15 @@ const currentDate = moment().format('MMMM Do YYYY')
 $('#currentDay').text(currentDate)
 const currentHour = moment().hour()
 
-$('.btn').on('click', function() {
+$('.btn').on('click', function () {
   const time = $(this).attr('id').split('_')[0]
   const input = $('#' + time + 'txt').val()
   localStorage.setItem(time, input)
 })
 
-for(let t = 9; t < 18; t++) {
-
+for (let t = 9; t < 18; t++) {
   let event = localStorage.getItem(t)
   $('#' + t + 'txt').val(event)
-  
   if (currentHour > t) {
     $('#' + t + 'txt').addClass('past')
   } else if (currentHour === t) {
