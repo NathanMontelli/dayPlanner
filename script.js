861f09247ea0952bb2de45ca821a4f32b7b0145a
Nathan Montelli <nathanmontelli@gmail.com>
2021-12-31 15:53:21 -0800

    added local storage

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const currentDate = moment().format('MMMM Do YYYY')
 $('#currentDay').text(currentDate)
 const currentHour = moment().hour()
-console.log(currentHour)
 
-for(let i = 9; i < 18; i++) {
-  if (currentHour < i) {
-    document.getElementById(i).classList.add('past')
-  } else if (currentHour === i) {
-    document.getElementById(i).classList.add('present')
+$('.btn').on('click', function() {
+  const time = $(this).attr('id').split('_')[0]
+  const input = $('#' + time + 'txt').val()
+  localStorage.setItem(time, input)
+})
+
+for(let t = 9; t < 18; t++) {
+
+  let event = localStorage.getItem(t)
+  $('#' + t + 'txt').val(event)
+  
+  if (currentHour > t) {
+    $('#' + t + 'txt').addClass('past')
+  } else if (currentHour === t) {
+    $('#' + t + 'txt').addClass('present')
   } else {
-    document.getElementById(i).classList.add('future')
+    $('#' + t + 'txt').addClass('future')
   }
 } 
